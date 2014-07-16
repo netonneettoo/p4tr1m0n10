5eb0b6a3f9e92bd23a5dba0b7f8f09fab43ef952
Walter Neto <jw_netao@hotmail.com>
2014-07-16 15:25:43 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,23 @@
 <?php
-  // teste...
+  class Db {
+    
+    private $server, $user, $pass, $db;
+    
+    function __construct($server, $user, $pass, $db) {
+      $this->server = $server;
+      $this->user = $user;
+      $this->pass = $pass;
+      $this->db = $db;
+    }
+    
+    function abrir() {
+      $conectar = mysql_connect($server, $user, $pass) or die (mysql_error());
+      return mysql_select_db($db, $conectar) or die (mysql_error());
+    }
+    
+    function fechar() {
+      mysql_close();
+    }
+    
+  }
 ?>
