9965ce08861aa319eaad9e5c184f21428c35e007
Walter Neto <jw_netao@hotmail.com>
2014-07-17 15:04:44 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,22 @@
 <?php
     class Config {
         
-        function fusoHorario() {
+        function setFusoHorario() {
             date_default_timezone_set('America/Argentina/Buenos_Aires');   
         }
         
-        function dinheiroBR($valor) {
+        function setDinheiroBR($valor) {
             //number_format(valor, casas decimais, separador decimal, separador milhar)
             return number_format($valor, 2, ',', '')
         }
         
-        function dinheiroEN($valor) {
+        function setDinheiroEN($valor) {
             //number_format(valor, casas decimais, separador decimal, separador milhar)
             return number_format($valor, 2, '.', '')
+        }
+        
+        function setMaiusculo() {
+            strtoupper(
         }
     }
 ?>
