3ac7b21e4efca24c402849a5b7baf616e9ceacac
Walter Neto <jw_netao@hotmail.com>
2014-07-17 15:01:41 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,18 @@
 <?php
-    date_default_timezone_set('America/Los_Angeles');
-    
+    class Config {
+        
+        function fusoHorario() {
+            date_default_timezone_set('America/Argentina/Buenos_Aires');   
+        }
+        
+        function dinheiroBR($valor) {
+            //number_format(valor, casas decimais, separador decimal, separador milhar)
+            return number_format($valor, 2, ',', '')
+        }
+        
+        function dinheiroEN($valor) {
+            //number_format(valor, casas decimais, separador decimal, separador milhar)
+            return number_format($valor, 2, '.', '')
+        }
+    }
 ?>
